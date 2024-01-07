d9f9f69703761056abb9f8f670fba560fa7c214c
herilala <hei.herilala@gmail.com>
2024-01-08 00:11:31 +0300

    feat: add big sum endpoint

---
+++ src/main/java/com/poja/bigsum/endpoint/rest/controller/health/SumController.java
@@ -0,0 +1,18 @@
+package com.poja.bigsum.endpoint.rest.controller.health;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigInteger;
+
+@RestController
+@AllArgsConstructor
+public class SumController {
+  @GetMapping(value = "/big-sum")
+  public String getTeachers(
+          @RequestParam BigInteger a,
+          @RequestParam BigInteger b) {
+    return a.add(b).toString();
+  }
+}
